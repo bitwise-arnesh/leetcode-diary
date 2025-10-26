726999da701bdd3bd255907fac6acb8bca4efc5c
bitwise-arnesh <arnesh2018@gmail.com>
2025-10-26 20:03:19 +0530

    Time: 0 ms (100.00%) | Memory: 41.7 MB (68.56%) - LeetSync

---
+++ 1764-maximum-repeating-substring/maximum-repeating-substring.java
@@ -0,0 +1,19 @@
+class Solution 
+{
+    public int maxRepeating(String sequence, String word) 
+    {
+        StringBuilder sb = new StringBuilder();
+        int count = 0;      
+        while(true)
+        {
+            sb.append(word);
+            if(sequence.contains(sb.toString()))
+            {
+                count++;
+            }
+            else
+                break;
+        } 
+        return count; 
+    }
+}
